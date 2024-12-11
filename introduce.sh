2cac188ad2784e7b78c04b129a94a4edc94ffded
Ubuntu <ubuntu@ip-172-31-20-254.ec2.internal>
2024-12-11 03:52:57 +0000

    Intro

---
+++ introduce.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name=Ainura
+age=52
+favourite_language=Linux
+hobby=dancing
+dream=travelling
+
+echo "Hi, my name is $name, and I'm $age years old. Every morning, $name likes to spend time $hobby. As a $age -year-old, I believe it's never to late to follow your dreams. My favourite programming language is $favourite_language .I code in $favourite_language almost every day! One day, I hope my love for $favourite_language will help me achive my dream of $dream. "When I am not busy working toward $dream, you will find me $hobby to relax." That's a little about me, $name - $age -year-old who loves $favourite_language , enjoy $hobby , and dreams of $dream !"
